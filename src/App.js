0c63f3ba7e226fafd44e1fc6b526c597e38969ce
Eder <chlrobert93@gmail.com>
2021-07-08 16:18:27 -0500

    Comunicacioń de la propuesta de elementos

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
-import react from 'react';
+import React from 'react';
+import Accordion from './components/Accordion';
 
+const items = [
+    {
+       title: 'What is React?',
+       content: 'React is a front end Javascript framework'
+    },
+
+    {
+        title: 'Why use React?',
+        cotent: 'React is a favorite JS library among engineers'
+    },
+
+    {
+        title: 'How do you use React?',
+        content: 'You use React by creating components'
+    }
+];
 
 export default () => {
-    return <h1>Widgets App</h1>;
-}
+    return(
+        <div>
+            <Accordion items={items}/>
+        </div>
+    );
+};
