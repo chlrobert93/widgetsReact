495c2e0011853611696997792a95614934455b4f
Eder <chlrobert93@gmail.com>
2021-07-09 15:45:18 -0500

    Implementar useState

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
+
 
 const Accordion = ({items}) => {
+    //Inicializar nuestro nuevo state
+    const [activeIndex, setActiveIndex] = useState(null);
 
-    const renderedItems = items.map((item) => {
+  const onTitleClick = (index) => {
+      //console.log('Title cliked', index);
+      setActiveIndex(index);
+  };
+
+    const renderedItems = items.map((item, index) => {
      return (
             <React.Fragment key={item.title}>
-                <div className='title active'>
+                <div className='title active' onClick={() => onTitleClick(index )}>
+                        
+                
+                
                     <i className= "dropdown icon"></i>
                 {item.title}
                 </div>
@@ -16,7 +27,10 @@ const Accordion = ({items}) => {
      ); 
  });
 
-     return <div className="ui styled accordion">{renderedItems}</div>;
+     return <div className="ui styled accordion">
+         {renderedItems}
+         <h1>{activeIndex}</h1>
+         </div>;
 };
 
 export default Accordion; 
