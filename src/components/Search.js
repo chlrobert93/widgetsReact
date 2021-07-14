20ee8bf157c4d82d91378ee8c8b3c46f2d284e43
Eder <chlrobert93@gmail.com>
2021-07-14 14:11:26 -0500

    Reparación de una advertencia

--- src/components/Search.js
+++ src/components/Search.js
@@ -6,62 +6,49 @@ const Search = () =>{
 
     //Neuvo state
     const [term, setTerm] = useState('programing'); 
+    const [debouncedTerm, setDebouncedTerm] = useState(term);
     const [results, setResults] =  useState([]);
 
      console.log(results);
 
+     /* */
+     useEffect(() => {
 
-     useEffect( () =>  {
-          const search = async () => {
-          //Conectar con locaholts 3001 y ejecutar node indendes.js
-          const {data} = await axios.get('https://en.wikipedia.org/w/api.php',{
-           params:{
-               action: 'query',
-               list: "search",
-               origin: '*',
-               format: 'json',
-               srsearch:term,
-            },
-        });
+      const timeId = setTimeout(() => {
+        setDebouncedTerm(term);
+      },1000);
 
-        setResults(data.query.search);
-     };
-
-   // const timeoutId = setTimeout(() => {
-     /*
-      if(term) {
-        search(); 
-     }
-     
-     }, 1000);
-*/
-
-console.log(term);
-console.log(results.length);
-     if (term && !results.length) {	
+      //Devolvera una función de limpieza que cancelará ese taimer
+      return () => {
+        clearTimeout(timeId);
+      };
        
-      search();	
-    } else {	
-      console.log('ddd');
-      const timeoutId = setTimeout(() => {	
-        if (term) {	
-          search();	
-        }	
-      }, 1000);
-   /*
-     console.log('Initial render or term was changed');
+     },[term]);
 
-     return () => {
-       console.log('CLEANUP');
-     };
-  */ 
-    //Se cansala ca vex que el usuario ingrsa un caracter el timepo asta el que deje de ingresar hse consulta
-    return () => {
-      clearTimeout(timeoutId);
-    };
-  }
-},[term]);
+     useEffect(() => {
+        const search = async () => {
+        //Conectar con locaholts 3001 y ejecutar node indendes.js
+        const {data} = await axios.get('https://en.wikipedia.org/w/api.php',{
+         params:{
+             action: 'query',
+             list: "search",
+             origin: '*',
+             format: 'json',
+             srsearch:debouncedTerm,
+          },
+      });
+      
+      setResults(data.query.search);
+   };
+      if(debouncedTerm){
+  
+         search();
 
+      }
+      //Queremos que se ejecute siempre que el compoenente se renderice por primera vez o cambie los terminos de rebote
+     },[debouncedTerm]);
+
+   
 
     const renderResults = results.map((result) => {
         return (
