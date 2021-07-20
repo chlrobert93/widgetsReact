c88ba4c01238ba6200b43dc505afa7fad8dc945a
Eder <chlrobert93@gmail.com>
2021-07-19 19:14:20 -0500

    Construcción del componente convert

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import Dropdown from './Dropdown'; 
+import Convert from './Convert';
 
 const options = [
     {
@@ -59,6 +60,9 @@ const Translate = () => {
                 </div>
             </div>
             <Dropdown label="Selected Language" selected={language} onSelectedChange={setLanguage} options={options} />
+            <hr />
+            <h3 className="ui header">Output</h3>
+            <Convert text={text} language={language} />           
         </div>
     )
 };
